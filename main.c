56d36163aad9748a078fc5bc9ce4f7e3cf04de56
Tarkin <tark@tarknet.com>
2022-07-14 14:56:11 -0500

    intial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//handle incoming argument vector
+int handleArgs(int argc, char **argv){
+
+
+
+}
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+
+}
